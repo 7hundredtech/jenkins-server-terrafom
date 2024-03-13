1ff7f70e88a722efe09044274d247ea9d85e151d
7hundredtech <ojosamuel_700@yahoo.com>
2024-03-13 11:36:18 +0100

    initial commit

---
+++ modules/iam/variables.tf
@@ -0,0 +1,4 @@
+variable "iam_policy_name" {}
+variable "iam_role_name" {}
+variable "iam_policy_attachment_name" {}
+variable "jenkins_instance_profile_name" {}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,2 @@
+variable "default_vpc_name" {}
+variable "default_subnet_name" {}
