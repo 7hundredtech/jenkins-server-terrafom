69b491c68c12f042c128611f05734a50d8b88a2e
7hundredtech <ojosamuel_700@yahoo.com>
2024-03-19 10:19:37 +0100

    update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -11,6 +11,18 @@ sudo systemctl status jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 sudo yum install git -y
+sudo yum install docker -y
+sudo service docker start
+sudo systemctl enable docker
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin
 
 sudo wget https://releases.hashicorp.com/terraform/1.3.7/terraform_1.3.7_linux_amd64.zip
 sudo unzip terraform_1.3.7_linux_amd64.zip
